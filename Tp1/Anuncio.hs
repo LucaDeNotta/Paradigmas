e77736a3e21b02f6d05080605813f1996113e19c
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-08-19 11:45:30 -0300

    agregarA y sacarA

--- Tp1/Anuncio.hs
+++ Tp1/Anuncio.hs
@@ -1,23 +1,34 @@
-module Anuncio ( Anuncio, nuevoA, nombreA, duracionA, departamentosA, agreagarA, sacarA, aplicaA )
+module Anuncio ( Anuncio, nuevoA, nombreA, duracionA, departamentosA, agregarA, sacarA, aplicaA )
   where
 
 import Tipos
 
 data Anuncio = Anu Nombre [ Departamento ] Duracion deriving (Eq, Show, Ord)
 
+--PREGUNTAR EMILIO
 nuevoA :: Nombre -> Duracion -> Anuncio         -- dado un nombre y una duracion en segundos retorna un nuevo Anuncio
-nuevoA name duration = (Anu name [] duration)
+nuevoA nombre duracion | null nombre || duracion <= 0 = error "Ingrese datos válidos"
+                       | otherwise                    = (Anu nombre [] duracion)
+
 nombreA :: Anuncio -> Nombre                    -- dado un anuncio retorna su nombre
-nombreA (Anu name _ _) = name
+nombreA (Anu nombre _ _) = nombre
+
 duracionA :: Anuncio -> Duracion                -- dado un anuncio retorna su duración
-duracionA (Anu _ _ duration) = duration
+duracionA (Anu _ _ duracion) = duracion
+
 departamentosA :: Anuncio -> [ Departamento ]   -- dado un anuncio retorna los departamentos que le fueron asociados
 departamentosA (Anu _ departamentos _) = departamentos
-agreagarA :: Departamento -> Anuncio -> Anuncio -- permite asignar un departamento a un anuncio
-agreagarA "" anuncio = anuncio
+
+agregarA :: Departamento -> Anuncio -> Anuncio -- permite asignar un departamento a un anuncio
+agregarA "" anuncio                                                        = error "Ingresar un departamento válido"
+agregarA departamento anuncio | elem departamento (departamentosA anuncio) = error "El departamento ingresado ya está asignado al anuncio"
+agregarA departamento (Anu nombre departamentos duracion)                  = (Anu nombre (departamento:departamentos) duracion)
+
+--PENSAR MENSAJE ERROR
 sacarA :: Departamento -> Anuncio -> Anuncio    -- permite quitarle un departamento a un anuncio
-sacarA depa (Anu nombre [] duracion) = (Anu nombre [] duracion)
+sacarA departamento anuncio | notElem departamento (departamentosA anuncio) = error "El departamento ingresado no está asignado al anuncio"
+
 aplicaA :: [ Departamento ] -> Anuncio -> Bool  -- responde si un anuncion debe emitirse para alguno de los departamentos consultados
 aplicaA _ (Anu _ [] _) = False
-aplicaA [] _ = False
+aplicaA [] _           = False
 aplicaA (x:xs) anuncio = elem x (departamentosA anuncio) || aplicaA xs anuncio
