7c3dea783663a892d7a225f3396c7217c26556a9
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-08-25 18:55:35 -0300

    Filesystem
    Hay que ver el tema de agregarDepartamentoF (si tenemos que chequear que, para agregar un departamento, tiene que estar al menos un anuncio del FS asociado al departamento). Para sacarDepartamentoF tenemos que ver si hay alguna restricción.

--- Tp1/Filesystem.hs
+++ Tp1/Filesystem.hs
@@ -7,10 +7,23 @@ import Anuncio
 data FileSystem = FS [Departamento] [Anuncio] deriving (Eq, Show)
 
 nuevoF :: FileSystem                                              -- permite obtener un nuevo FileSystem
+nuevoF = (FS [] [])
+
 departamentosF :: FileSystem -> [ Departamento ]                  -- dado un FileSystem retorna los departamentos que incluye
+departamentosF (FS departamentos _) = departamentos
+
 anunciosF :: FileSystem -> [ Anuncio ]                            -- dado un FileSystem retorna los anuncios que incluye
-agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem            -- permite agregar un anuncio  
+anunciosF (FS _ anuncios) = anuncios
+
+agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem            -- permite agregar un anuncio
+agregarAnuncioF anuncio (FS departamentos anuncios) | elem anuncio anuncios = error "El anuncio ya se encuentra en el FileSystem"
+                                                    | otherwise             = (FS departamentos (anuncio:anuncios))
+
 sacarAnuncioF :: Anuncio -> FileSystem -> FileSystem              -- permite eliminar un anuncio
-agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem  -- permite agregar un departamento
-sacarDepartamentoF :: Departamento -> FileSystem -> FileSystem    -- permite eliminar un departamento
-anunciosParaF :: [Departamento] -> FileSystem -> [Anuncio]        -- entrega los anuncios a emitir para un conjunto de departamentos
+sacarAnuncioF anuncio (FS departamentos anuncios) | notElem anuncio anuncios = error "El anuncio ingresado no se encuentra en el Filesystem"
+                                                  | otherwise                = (FS departamentos (filter (/=anuncio) anuncios))
+
+--CORREGIR LUCA. PREGUNTAR EMILIO
+agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem  -- permite agregar un departamento 
+agregarDepartamentoF departamento (FS departamentos anuncios) | elem departamento departamentos = error "El departamento ingresado ya se encuentra en el Filesystem"
+                                                              | otherwise                       = (FS (departamento:departamentos) anuncios)
