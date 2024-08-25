26da1ad2e8245d3998a78ab3aa43e4119b07835e
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-08-25 18:56:59 -0300

    nuevoP, archivosR y departamentosP

--- Tp1/Prompter.hs
+++ Tp1/Prompter.hs
@@ -5,13 +5,19 @@ import Tipos
 import Anuncio
 import FileSystem
 
-data Prompter = Pro FileSystem  [Departamento] Int deriving (Eq, Show)
+data Prompter = Pro FileSystem [Departamento] Int deriving (Eq, Show)
 
 nuevoP :: FileSystem -> Prompter                       -- permite obtener un nuevo Prompter en base a un FileSystem
+nuevoP fileSystem = (Pro fileSystem [] 0)
+
 archivosR :: Prompter -> FileSystem                    -- dado un prompter retorna su fileSystem
+archivosR (Pro fileSystem _ _) = fileSystem
+
 departamentosP :: Prompter -> [Departamento]           -- dado un prompter retorna sus departamentos
+departamentosP (Pro _ departamentos _) = departamentos
+
 configurarP :: Prompter -> [Departamento] -> Prompter  -- Prepara el prompter para emitir los anuncios en los departementos indicados
 nunciosP :: Prompter ->  [Nombre]                      -- entrega la lista de nombres de anuncios configurados
-showP :: Prompter -> Anuncio                           -- muestra el anuncio actual 
+showP :: Prompter -> Anuncio                           -- muestra el anuncio actual
 avanzarP :: Prompter -> Prompter                       -- pasa al siguiente anuncio
-duracionP :: Prompter -> Duracion                      -- indica la duracion total de los anuncios configurados  
+duracionP :: Prompter -> Duracion                      -- indica la duracion total de los anuncios configurados
