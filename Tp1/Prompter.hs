f9adaf98cdd41ffa2dd74560ca259fb3f5cecb68
LucaDeNotta <141259976+LucaDeNotta@users.noreply.github.com>
2024-08-29 21:06:31 -0300

    Terminamos Prompter

--- Tp1/Prompter.hs
+++ Tp1/Prompter.hs
@@ -17,7 +17,19 @@ departamentosP :: Prompter -> [Departamento]           -- dado un prompter retor
 departamentosP (Pro _ departamentos _) = departamentos
 
 configurarP :: Prompter -> [Departamento] -> Prompter  -- Prepara el prompter para emitir los anuncios en los departementos indicados
-nunciosP :: Prompter ->  [Nombre]                      -- entrega la lista de nombres de anuncios configurados
+configurarP _ []                                                = error "Ingrese una lista de departamentos no vacia" 
+configurarP (Pro fileSystem pDepartamentos index) departamentos = (Pro fileSystem departamentos index)
+
+anunciosP :: Prompter ->  [Nombre]                      -- entrega la lista de nombres de anuncios configurados
+anunciosP (Pro fileSystem departamentos _) = [(nombreA anuncio) | anuncio <- (anunciosParaF departamentos fileSystem)]
+
 showP :: Prompter -> Anuncio                           -- muestra el anuncio actual
+showP (Pro fileSystem departamentos index) | null anuncios =  error "No hay anuncios asignados a los departamentos del Prompter"
+                                           | otherwise     = anuncios !! index
+                                            where anuncios  = anunciosParaF departamentos fileSystem
+
 avanzarP :: Prompter -> Prompter                       -- pasa al siguiente anuncio
+avanzarP (Pro fileSystem departamentos index) = (Pro fileSystem departamentos (mod (index + 1) (length (anunciosParaF departamentos fileSystem))))
+
 duracionP :: Prompter -> Duracion                      -- indica la duracion total de los anuncios configurados
+duracionP (Pro fileSystem departamentos _) = sum [(duracionA anuncio) | anuncio <- (anunciosParaF departamentos fileSystem)]
