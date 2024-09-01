2d878c691737d3c7a13bd019b9d9e45a233e4581
LucaDeNotta <141259976+LucaDeNotta@users.noreply.github.com>
2024-09-01 16:49:16 -0300

    Rehacer funciones para agregar coherencia

--- Tp1/Anuncio.hs
+++ Tp1/Anuncio.hs
@@ -1,14 +1,14 @@
 module Anuncio ( Anuncio, nuevoA, nombreA, duracionA, departamentosA, agregarA, sacarA, aplicaA )
   where
 
-import Tipos ( Departamento, Duracion, Nombre )
+import Tipos 
 
 data Anuncio = Anu Nombre [ Departamento ] Duracion deriving (Eq, Show, Ord)
 
 --PREGUNTAR EMILIO
-nuevoA :: Nombre -> Duracion -> Anuncio         -- dado un nombre y una duracion en segundos retorna un nuevo Anuncio
-nuevoA "" _                            = error "Ingrese un nombre válido" 
-nuevoA nombre duracion | duracion <= 0 = error "Ingrese una duración válida"
+nuevoA :: Nombre -> Duracion -> Anuncio         -- dado un nombre y una duracion en segundos retorna un nuevo Anuncio 
+nuevoA nombre duracion | null nombre   = error "Ingrese un nombre válido"
+                       | duracion <= 0 = error "Ingrese una duración válida"
                        | otherwise     = (Anu nombre [] duracion)
 
 nombreA :: Anuncio -> Nombre                    -- dado un anuncio retorna su nombre
@@ -21,15 +21,18 @@ departamentosA :: Anuncio -> [ Departamento ]   -- dado un anuncio retorna los d
 departamentosA (Anu _ departamentos _) = departamentos
 
 agregarA :: Departamento -> Anuncio -> Anuncio -- permite asignar un departamento a un anuncio
-agregarA "" _                                                              = error "Ingresar un departamento válido"
-agregarA departamento anuncio | elem departamento (departamentosA anuncio) = error "El departamento ingresado ya está asignado al anuncio"
-agregarA departamento (Anu nombre departamentos duracion)                  = (Anu nombre (departamento:departamentos) duracion)
+agregarA departamento (Anu nombre departamentos duracion) | null departamento               = error "Ingresar un departamento válido"
+                                                          | elem departamento departamentos = error "El departamento ingresado ya está asignado al anuncio"
+                                                          | otherwise                       = (Anu nombre (departamento:departamentos) duracion)
 
 sacarA :: Departamento -> Anuncio -> Anuncio    -- permite quitarle un departamento a un anuncio
 sacarA departamento (Anu nombre departamentos duracion) | notElem departamento departamentos = error "El departamento ingresado no está asignado al anuncio"
                                                         | otherwise                          =  (Anu nombre (filter (/=departamento) departamentos) duracion)
 
-aplicaA :: [ Departamento ] -> Anuncio -> Bool  -- responde si un anuncio debe emitirse para alguno de los departamentos consultados
-aplicaA _ (Anu _ [] _) = error "La lista de departamentos del anuncio está vacía"
-aplicaA [] _           = False
-aplicaA (departamento:departamentos) anuncio = elem departamento (departamentosA anuncio) || aplicaA departamentos anuncio
+aplicaA :: [Departamento] -> Anuncio -> Bool    -- responde si un anuncio debe emitirse para alguno de los departamentos consultados
+aplicaA departamentos (Anu nombre departamentosA duracion) | null departamentosA     = error "La lista de departamentos del anuncio está vacía"
+                                                           | null departamentos      = False
+                                                           | otherwise               = elem departamento departamentosA || aplicaA tailDepartamentos anuncio
+                                                             where anuncio           = (Anu nombre departamentosA duracion)
+                                                                   departamento      = head departamentos
+                                                                   tailDepartamentos = tail departamentos
