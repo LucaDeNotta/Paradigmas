9830e8b71f55b30e8cfaa26d09454dc80769e25c
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-09-20 20:18:29 -0300

    Emprolijar Tree

--- TP 2/Tree.java
+++ TP 2/Tree.java
@@ -11,85 +11,44 @@ public class Tree{
     private Object value;
 
     public Tree( Object a ) {
-        left = new NotConnectedArista(nadaALaSiniestra);
-        right = new NotConnectedArista(nadaALaDiestra);
+        left = new NotConnectedArista( nadaALaSiniestra );
+        right = new NotConnectedArista( nadaALaDiestra );
         value = a;
     }
 
     public List dfs() {
         List list = new ArrayList();
-        dfsHelper(list);
-//        Deque<Tree> stack = new ArrayDeque();
-//        stack.push(this);
-//        while (!stack.isEmpty()) {
-//            Tree node = stack.pop();
-//            list.add( node.carga() );
-//            if (node.right != null) {
-//            stack.push(node.right);
-//            }
-//            if (node.left != null) {
-//            stack.push(node.left);
-//            }
-//        }
+        dfsHelper( list );
         return list;
     }
 
-    public void dfsHelper(List list) {
-        list.add(value);
-        left.subTreeDFS(list);
-        right.subTreeDFS(list);
+    public void dfsHelper( List list ) {
+        list.add( value );
+        left.subTreeDFS( list );
+        right.subTreeDFS( list );
     }
 
     public List bfs() {
         List list = new ArrayList();
         Queue<Tree> queue = new ArrayDeque();
-        //bfsQueue( queue );
         queue.add( this );
-        //bfsHelper( list, queue );
-        while (!queue.isEmpty()) {
+        while ( !queue.isEmpty() ) {
             Tree actual = queue.poll();
             list.add( actual.value );
             actual.left.subTreeBFS( list, queue );
             actual.right.subTreeBFS( list, queue );
 
         }
-//        queue.add(this);
-//        while (!queue.isEmpty()) {
-//            Tree node = queue.remove();
-//            list.add( node.carga() );
-//            if (node.left != null) {
-//                queue.add(node.left);
-//            }
-//            if (node.right != null) {
-//                queue.add(node.right);
-//            }
-//        }
         return list;
     }
 
-
-    //public void bfsHelper( List list, Queue<Tree> queue ) {
-        //Tree actual = queue.poll();
-            //list.add( actual.value );
-            //actual.bfsQueue( list, queue );
-            //actual.left.subTreeBFS( list, queue );
-            //actual.right.subTreeBFS( list, queue );
-
-    //}
-
-    //public void bfsQueue( List list, Queue<Tree> queue ) {
-        //left.subTreeBFS( list, queue );
-        //right.subTreeBFS( list, queue );
-        //bfsHelper( list, queue );
-    //}
-
     public Tree atLeft( Tree b ) {
-        left = left.setTree(b);
+        left = left.setTree( b );
         return this;
     }
 
     public Tree atRight( Tree b ) {
-        right = right.setTree(b);
+        right = right.setTree( b );
         return this;
     }
     public Object carga() {
