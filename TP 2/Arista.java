0bd80770f68bae6208c3e5aadd830c43a6b815e2
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-09-20 20:21:15 -0300

    Emprolijar Arista

--- TP 2/Arista.java
+++ TP 2/Arista.java
@@ -4,12 +4,12 @@ import java.util.List;
 import java.util.Queue;
 
 public abstract class Arista {
-    public abstract Arista setTree(Tree tree);
+    public abstract Arista setTree( Tree tree );
 
     public abstract Tree connectedTree();
 
-    public abstract void subTreeDFS(List list);
+    public abstract void subTreeDFS( List list );
 
-    public abstract void subTreeBFS(List list, Queue<Tree> queue);
+    public abstract void subTreeBFS( List list, Queue<Tree> queue );
 
 }
