9b55551e147bcdab9114c8ba4b378a2c2c079222
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-09-22 23:07:35 -0300

    Nombres variables

--- TP 2/ConnectedArista.java
+++ TP 2/ConnectedArista.java
@@ -6,16 +6,16 @@ import java.util.Queue;
 public class ConnectedArista extends Arista {
     private Tree connectedTree;
 
-    public ConnectedArista( Tree tree ) {
+    public ConnectedArista( Tree aTree ) {
         connectedTree = tree;
     }
 
     public Tree connectedTree() { return connectedTree; }
 
-    public void subTreeDFS( List list ) { connectedTree.dfsHelper( list ); }
+    public void subTreeDFS( List aList ) { connectedTree.dfsHelper( aList ); }
 
-    public void subTreeBFS( Queue<Tree> queue ) {
-        queue.add(connectedTree);
+    public void subTreeBFS( Queue<Tree> aQueue ) {
+        queue.add( connectedTree );
     }
 
 }
