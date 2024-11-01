c3178f4f862d5658f383406f231ca24f1395f43d
LucaDeNotta <141259976+LucaDeNotta@users.noreply.github.com>
2024-11-01 15:57:47 -0300

    Update RoverTest.java

--- TP 3/RoverTest.java
+++ TP 3/RoverTest.java
@@ -9,26 +9,26 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class RoverTest {
     // TODO cambiar somewhere
     @Test public void newRoverPointsSomewhere(){
-        assertTrue(new Rover(pos_inicial(), norte()).apuntaDireccion(norte()));
+        assertTrue(new Rover(posicionInicial(), norte()).apuntaDireccion(norte()));
     }
 
     // TODO organizar la creacion de los Rover y cuando sea necesario, organizar el codigo
     @Test public void RoverMovesForward(){
-        assertTrue(new Rover(pos_inicial(), norte()).ejecutarComando('f').estaUbicacion(1,2));
-        assertTrue(new Rover(pos_inicial(), este()).ejecutarComando('f').estaUbicacion(2,1));
-        assertTrue(new Rover(pos_inicial(), sur()).ejecutarComando('f').estaUbicacion(1,0));
-        assertTrue(new Rover(pos_inicial(), oeste()).ejecutarComando('f').estaUbicacion(0,1));
+        assertTrue(new Rover(posicionInicial(), norte()).ejecutarComando('f').estaUbicacion(1,2));
+        assertTrue(new Rover(posicionInicial(), este()).ejecutarComando('f').estaUbicacion(2,1));
+        assertTrue(new Rover(posicionInicial(), sur()).ejecutarComando('f').estaUbicacion(1,0));
+        assertTrue(new Rover(posicionInicial(), oeste()).ejecutarComando('f').estaUbicacion(0,1));
     }
 
     @Test public void RoverMovesBackward(){
-        assertTrue(new Rover(pos_inicial(), norte()).ejecutarComando('b').estaUbicacion(1,0));
-        assertTrue(new Rover(pos_inicial(), este()).ejecutarComando('b').estaUbicacion(0,1));
-        assertTrue(new Rover(pos_inicial(), sur()).ejecutarComando('b').estaUbicacion(1,2));
-        assertTrue(new Rover(pos_inicial(), oeste()).ejecutarComando('b').estaUbicacion(2,1));
+        assertTrue(new Rover(posicionInicial(), norte()).ejecutarComando('b').estaUbicacion(1,0));
+        assertTrue(new Rover(posicionInicial(), este()).ejecutarComando('b').estaUbicacion(0,1));
+        assertTrue(new Rover(posicionInicial(), sur()).ejecutarComando('b').estaUbicacion(1,2));
+        assertTrue(new Rover(posicionInicial(), oeste()).ejecutarComando('b').estaUbicacion(2,1));
     }
 
     @Test public void RoverRotatesLeft(){
-        Rover rover = new Rover(pos_inicial(), norte());
+        Rover rover = new Rover(posicionInicial(), norte());
         assertTrue(rover.recibirComandos("llll").apuntaDireccion(norte()));
         assertTrue(rover.ejecutarComando('l').apuntaDireccion(oeste()));
         assertTrue(rover.ejecutarComando('l').apuntaDireccion(sur()));
@@ -37,7 +37,7 @@ public class RoverTest {
     }
 
     @Test public void RoverRotatesRight(){
-        Rover rover = new Rover(pos_inicial(), norte());
+        Rover rover = new Rover(posicionInicial(), norte());
         assertTrue(rover.recibirComandos("rrrr").apuntaDireccion(norte()));
         assertTrue(rover.ejecutarComando('r').apuntaDireccion(este()));
         assertTrue(rover.ejecutarComando('r').apuntaDireccion(sur()));
@@ -46,72 +46,73 @@ public class RoverTest {
     }
 
     @Test public void canOpenEscotillaSuperior(){
-        assertTrue(new Rover(pos_inicial(), norte()).ejecutarComando('O').isEscotillaSuperiorOpen());
+        assertTrue(new Rover(posicionInicial(), norte()).ejecutarComando('O').isEscotillaSuperiorOpen());
     }
 
     @Test public void canOpenEscotillaInferior(){
-        assertTrue(new Rover(pos_inicial(), norte()).ejecutarComando('o').isEscotillaInferiorOpen());
+        assertTrue(new Rover(posicionInicial(), norte()).ejecutarComando('o').isEscotillaInferiorOpen());
     }
 
     @Test public void canAspirar(){
-        Rover rover = new Rover(pos_inicial(), norte()).ejecutarComando('O');
+        Rover rover = new Rover(posicionInicial(), norte()).ejecutarComando('O');
         assertEquals(rover, rover.ejecutarComando('a'));
     }
 
     @Test public void canCavar(){
-        Rover rover = new Rover(pos_inicial(), norte()).ejecutarComando('o');
-        assertEquals(rover, rover.ejecutarComando('i'));
+        Rover rover = new Rover( posicionInicial(), norte() ).ejecutarComando('o');
+        assertEquals( rover, rover.ejecutarComando('i') );
     }
 
     @Test public void cantOpenBothEscotillas(){
         assertThrowsLike(noSePuedenAbrirLasDosEscotillas,
-                () -> new Rover(pos_inicial(), norte()).ejecutarComando('o').ejecutarComando('O'));
+                () -> new Rover(posicionInicial(), norte()).ejecutarComando('o').ejecutarComando('O'));
     }
 
     @Test public void cantCloseClosedEscotillas(){
-        assertThrowsLike(noSePuedenCerrarEscotillasCerradas,
-                () -> new Rover(pos_inicial(), norte()).ejecutarComando('c'));
+        assertThrowsLike( noSePuedenCerrarEscotillasCerradas,
+                () -> new Rover(posicionInicial(), norte()).ejecutarComando('c') );
     }
 
     @Test public void cantAspirarIfEscotillaClosed(){
-        assertThrowsLike(escotillaCerrada,
-                () -> new Rover(pos_inicial(), norte()).ejecutarComando('a'));
+        assertThrowsLike( escotillaCerrada,
+                () -> new Rover( posicionInicial(), norte()).ejecutarComando('a') );
     }
 
     @Test public void cantCavarIfEscotillaClosed(){
         assertThrowsLike(escotillaCerrada,
-                () -> new Rover(pos_inicial(), norte()).ejecutarComando('i'));
+                () -> new Rover( posicionInicial(), norte() ).ejecutarComando('i'));
     }
 
     @Test public void forwardAndTurnRightComands(){
-        Rover rover = new Rover(pos_inicial(), norte()).recibirComandos("ffrfffrrffff");
-        assertTrue(rover.apuntaDireccion(oeste()));
-        assertTrue(rover.estaUbicacion(0,3));
+        Rover rover = new Rover( posicionInicial(), norte() ).recibirComandos("ffrfffrrffff");
+        assertTrue( rover.apuntaDireccion(oeste()) );
+        assertTrue( rover.estaUbicacion(0,3) );
     }
 
     @Test public void backwardAndTurnLeftComands(){
-        Rover rover = new Rover(pos_inicial() , norte()).recibirComandos("blbbllbbbb");
-        assertTrue(rover.apuntaDireccion(este()));
-        assertTrue(rover.estaUbicacion(-1,0));
+        Rover rover = new Rover( posicionInicial(), norte() ).recibirComandos("blbbllbbbb");
+        assertTrue( rover.apuntaDireccion( este() ) );
+        assertTrue( rover.estaUbicacion(-1,0) );
     }
 
     @Test public void forwardBackwardLeftRightComands(){
-        Rover rover = new Rover(pos_inicial(), norte()).recibirComandos("fffrbrblfffrb");
-        assertTrue(rover.apuntaDireccion(sur()));
-        assertTrue(rover.estaUbicacion(3,6));
+        Rover rover = new Rover( posicionInicial(), norte() ).recibirComandos("fffrbrblfffrb");
+        assertTrue( rover.apuntaDireccion(sur()) );
+        assertTrue( rover.estaUbicacion(3,6) );
     }
 
     @Test public void aspirarCavarAbrirCerrarEscotillasComands(){
-        Rover rover = new Rover(pos_inicial(), norte());
-        assertEquals(rover, rover.recibirComandos("Oacoic"));
+        Rover rover = new Rover( posicionInicial(), norte() );
+        assertEquals( rover, rover.recibirComandos("Oacoic") );
+        assertTrue( rover.estaUbicacion(1,1) );
     }
 
     @Test public void notProcessFollowingComands(){
-        Rover rover = new Rover(pos_inicial(), norte());
-        assertThrowsLike(String.format(noSePuedeEjecutarComando, 'z'),
-                () -> rover.recibirComandos("frfllbzbbf"));
-        assertTrue(rover.apuntaDireccion(oeste()));
-        assertTrue(rover.estaUbicacion(3,2));
+        Rover rover = new Rover( posicionInicial(), norte() );
+        assertThrowsLike( String.format(noSePuedeEjecutarComando, 'z'),
+                () -> rover.recibirComandos("frfllbzbbf") );
+        assertTrue(rover.apuntaDireccion( oeste()) );
+        assertTrue( rover.estaUbicacion(3,2) );
     }
 
     private static void assertThrowsLike( String expectedMsg, Executable expression ){
@@ -133,8 +134,8 @@ public class RoverTest {
     public Direccion oeste(){
         return new Oeste();
     }
-// TODO cambiar lo que haga falta a camelCase 
-    public Posicion pos_inicial(){
+
+    public Posicion posicionInicial(){
         return new Posicion(1, 1);
     }
 }
