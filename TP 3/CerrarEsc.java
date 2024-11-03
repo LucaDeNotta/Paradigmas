66a3a5501fee235f386c5952c3f1a0d186d449b2
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-11-03 15:53:54 -0300

    Constructor CerrarEsc.java

--- TP 3/CerrarEsc.java
+++ TP 3/CerrarEsc.java
@@ -1,9 +1,10 @@
 package Rover;
 
 public class CerrarEsc extends Comando{
-    {
+    public CerrarEsc(){
         comando = 'c';
     }
+
     public void ejecutar(Rover rover) {
         rover.cerrarEsc();
     }
