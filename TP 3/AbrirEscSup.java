afff691f4ad2579d1295b9480883e8c606c6b447
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-11-03 15:52:38 -0300

    Constructor AbrirEscSup.java

--- TP 3/AbrirEscSup.java
+++ TP 3/AbrirEscSup.java
@@ -1,9 +1,10 @@
 package Rover;
 
 public class AbrirEscSup extends Comando {
-    {
+    public AbrirEscSup(){
         comando = 'O';
     }
+
     public void ejecutar(Rover rover) {
         rover.abrirEscSup();
     }
