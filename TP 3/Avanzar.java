6be53457ee99ae662bd8ab1b86d5c75d821d0033
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-11-03 15:50:44 -0300

    Constructor Avanzar.java

--- TP 3/Avanzar.java
+++ TP 3/Avanzar.java
@@ -1,11 +1,11 @@
 package Rover;
 
 public class Avanzar extends Comando {
-    {
+    public Avanzar(){
         comando = 'f';
     }
+
     public void ejecutar(Rover rover) {
         rover.avanzar();
     }
-
 }
