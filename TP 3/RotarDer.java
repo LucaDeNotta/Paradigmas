8496c815620ee4590f1eee1a8922edb5bb87a2ce
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-11-03 15:52:09 -0300

    Constructor RotarDer.java

--- TP 3/RotarDer.java
+++ TP 3/RotarDer.java
@@ -1,9 +1,10 @@
 package Rover;
 
 public class RotarDer extends Comando {
-    {
+    public RotarDer() {
         comando = 'r';
     }
+
     public void ejecutar(Rover rover) {
         rover.rotarDer();
     }
