c6812332812b36ac36445a0fd133ba0a74d98438
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-11-03 15:54:33 -0300

    Constructor Aspirar.java

--- TP 3/Aspirar.java
+++ TP 3/Aspirar.java
@@ -1,9 +1,10 @@
 package Rover;
 
 public class Aspirar extends Comando {
-    {
+    public Aspirar(){
         comando = 'a';
     }
+
     public void ejecutar(Rover rover) {
         rover.aspirar();
     }
