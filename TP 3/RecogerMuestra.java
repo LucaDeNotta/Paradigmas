ae879253d648cabc27f8458dc7430d78c4dd515a
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-11-03 15:55:01 -0300

    Constructor RecogerMuestra.java

--- TP 3/RecogerMuestra.java
+++ TP 3/RecogerMuestra.java
@@ -1,9 +1,10 @@
 package Rover;
 
 public class RecogerMuestra extends Comando {
-    {
+    public RecogerMuestra() {
         comando = 'i';
     }
+
     public void ejecutar(Rover rover) {
         rover.recogerMuestra();
     }
