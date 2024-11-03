31a48fdcb5eb4c074d48b6e61985e7f6a80044c0
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-11-03 15:53:05 -0300

    Constructor AbrirEscInf.java

--- TP 3/AbrirEscInf.java
+++ TP 3/AbrirEscInf.java
@@ -1,9 +1,10 @@
 package Rover;
 
 public class AbrirEscInf extends Comando {
-    {
+    public AbrirEscInf(){
         comando = 'o';
     }
+
     public void ejecutar(Rover rover) {
         rover.abrirEscInf();
     }
