de43182ef81d7a3b6839c221b7070a648098be81
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-11-03 21:05:04 -0300

    Paréntesis Direccion

--- TP 3/Direccion.java
+++ TP 3/Direccion.java
@@ -5,9 +5,9 @@ public abstract class Direccion {
 
     public abstract Direccion left();
 
-    public abstract Posicion forward(Posicion posicion);
+    public abstract Posicion forward( Posicion posicion );
 
-    public abstract Posicion backward(Posicion posicion);
+    public abstract Posicion backward( Posicion posicion );
 
     public boolean equals( Object anObject ) {
         return this.getClass().isInstance( anObject );
