3cb23b443d34091234880a9d541f94e19c1de33a
LucaDeNotta <141259976+LucaDeNotta@users.noreply.github.com>
2024-12-02 04:15:14 -0300

    Delete TP 4/JuegoTerminado.java

--- TP 4/JuegoTerminado.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-import java.util.Stack;
-
-public class JuegoTerminado extends Juego{
-    public JuegoTerminado(List<Jugador> jugadores, Stack<Carta> mazo) {
-        super(jugadores, mazo);
-    }
-}
