f3073d174173958ce62440073bd58a1aa19445fd
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-12-02 01:11:18 -0300

    Update Juego.java

--- TP 4/Juego.java
+++ TP 4/Juego.java
@@ -1,66 +1,43 @@
+package nogracias;
+
 import java.util.List;
 import java.util.Stack;
 
-public class Juego {
+public  class Juego {
+
+    public static String cantidadDeFichasInvalida = "Cantidad de fichas invalida";
     public static String cantidadDeJugadoresNoEsCorrecto = "Cantidad de jugadores no es correcta";
 
-    int turnoActual = 0;
-    List<Jugador> jugadores;
+    Jugador jugadorActual;
     Stack<Carta> mazo;
-    Carta cartaActual;
-    Jugador ganador;
 
-    public Juego (List<Jugador> jugadores, Stack<Carta> mazo){
-        Round rondaSeleccionada = Round.listaRounds().stream()
-                .filter(ronda -> ronda.puedenJugar(jugadores.size()))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException(cantidadDeJugadoresNoEsCorrecto));
-
-        this.jugadores = jugadores;
-        rondaSeleccionada.repartir(this);
-
-        this.mazo = mazo;
-        cartaActual = mazo.pop();
-    }
-
-    public Juego avanzarTurno(){
-        if( !mazo.isEmpty() ){
-            turnoActual = ( turnoActual + 1 ) % jugadores.size();
-            return this;
+    public Juego (Stack<Carta> mazo, int fichasARepartir, List<String> jugadores){
+        if (jugadores.size() < 3 || jugadores.size() > 7){
+            throw new RuntimeException(cantidadDeJugadoresNoEsCorrecto);
         }
-        return terminarJuego();
+        if (fichasARepartir < 0){
+            throw new RuntimeException(cantidadDeFichasInvalida);
+        }
+
+//        jugadorActual = jugadores.stream()
+//                .map(jugador -> new JugadorActivo(jugador, fichasARepartir, ) {
+//                });
+        this.jugadorActual = new JugadorActivo(jugadores.getFirst(), fichasARepartir);
+        this.mazo = mazo;
     }
 
-    public Juego terminarJuego() {
-        //TODO problema cuando hay un empate
-        ganador = jugadores.stream()
-                .max((j1, j2) -> Integer.compare(j1.puntaje(), j2.puntaje())) // Compara puntajes
-                .orElse(null);
+    public Juego tomarCarta(String jugador){
+        jugadorActual.tomarCarta( jugador, mazo.pop() );
         return this;
     }
 
-
-    public Juego agarrarCarta(){
-        getJugadorActual().agarrarCarta( cartaActual );
-        cartaActual = mazo.pop();
-        return this;
-    }
-
-    public Juego usarFicha(){
-        getJugadorActual().usarFicha();
-        cartaActual.usarFicha();
-        avanzarTurno();
+    public Juego pagarFicha(String jugador){
+        jugadorActual.pagarFicha(jugador, mazo.peek());
 
         return this;
     }
 
     public Jugador getJugadorActual() {
-        return jugadores.get( turnoActual );
+        return jugadorActual;
     }
-
-    public Jugador getGanador() {
-        return ganador;
-    }
-
-    public void repartirFichas(int fichas) { jugadores.forEach(jugador -> jugador.asignarFichas(fichas)); }
 }
