0f843c1a1506c0c5a907c154760246b741b8a8e6
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-12-02 01:12:05 -0300

    Update Jugador.java

--- TP 4/Jugador.java
+++ TP 4/Jugador.java
@@ -1,42 +1,42 @@
-import java.util.ArrayList;
+package nogracias;
+
 import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.IntStream;
 
-public class Jugador {
-    private String nombre;
-    private int fichas = 0;
-    private List<Carta> cartas =  new ArrayList<Carta>();;
+public abstract class Jugador {
+    protected String nombre;
+    protected Integer fichas;
+    protected TreeSet<Carta> cartas = new TreeSet<>();
 
-    public Jugador(String nombre) { this.nombre = nombre; }
-
-    public Jugador asignarFichas(int fichas) {
+    public Jugador(String nombre, Integer fichas) {
+        this.nombre = nombre;
         this.fichas = fichas;
-        return this;
     }
 
-    public Jugador agarrarCarta(Carta carta) {
-        cartas.add(carta);
-        fichas += carta.fichas();
-        return this;
+    public String nombre(){
+        return nombre;
     }
 
-    public Jugador usarFicha(){
-        if( fichas == 0 ){
-            throw new RuntimeException("No hay fichas");
-        }
-        fichas--;
-        return this;
+    public Integer fichas(){
+        return fichas;
     }
 
-    public int puntaje(){
+    public abstract Jugador pagarFicha(String jugador, Carta carta);
+
+    public abstract Jugador tomarCarta(String jugador ,Carta carta);
+
+    public Integer puntaje(){
         List<Integer> valores = cartas.stream()
                 .map(Carta::value)
                 .toList();
 
-        return valores.stream()
-                .filter(each -> !valores.contains(each - 1))
-                .reduce(0, Integer::sum) - fichas;
+        return IntStream.range(0, valores.size())
+                .filter(i -> i == 0 || valores.get(i) != valores.get(i - 1) + 1)
+                .mapToObj(valores::get)
+                .mapToInt(Integer::intValue)
+                .sum() - fichas;
     }
-
-    public int fichas() { return fichas; }
+    public abstract Jugador ganador(Juego juego);
 
 }
