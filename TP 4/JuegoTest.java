f0fb34b3b1a04ac7c02428a27fb2549a52832c5d
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2024-12-02 01:10:54 -0300

    Update JuegoTest.java

--- TP 4/JuegoTest.java
+++ TP 4/JuegoTest.java
@@ -1,3 +1,6 @@
+package nogracias;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
@@ -11,40 +14,47 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class JuegoTest {
 
     @Test public void juegoConMenosDe3Jugadores() {
-        assertThrowsLike(Juego.cantidadDeJugadoresNoEsCorrecto, () -> new Juego( List.of(jugador1()), mazo5Cartas() ) );
+        assertThrowsLike(Juego.cantidadDeJugadoresNoEsCorrecto, () -> new Juego(
+                mazo5Cartas(),
+                2,
+                List.of(jugador1())) );
     }
 
     @Test public void juegoConMasDe7Jugadores() {
-        List<Jugador> jugadores = sieteJugadores();
-        jugadores.add(new Jugador("jugador8"));
-        assertThrowsLike(Juego.cantidadDeJugadoresNoEsCorrecto, () -> new Juego( jugadores, mazo5Cartas() ) );
+        List<String> jugadores = sieteJugadores();
+        jugadores.add("jugador8");
+        assertThrowsLike(Juego.cantidadDeJugadoresNoEsCorrecto,
+                () -> new Juego( 
+                                mazo5Cartas(),
+                            2 , 
+                            jugadores) );
     }
 
     @Test public void cantidadFichasIniciales3Jugadores(){
-        assertEquals(11, juego3Jugadores().getJugadorActual().fichas());
+        Assertions.assertEquals(11, juego3Jugadores().getJugadorActual().fichas());
     }
 
     @Test public void cantidadFichasIniciales6Jugadores(){
-        assertEquals(9, juego6Jugadores().getJugadorActual().fichas());
+        Assertions.assertEquals(9, juego6Jugadores().getJugadorActual().fichas());
     }
 
     @Test public void cantidadFichasIniciales7Jugadores(){
-        assertEquals(7, juego7Jugadores().getJugadorActual().fichas());
+        Assertions.assertEquals(7, juego7Jugadores().getJugadorActual().fichas());
     }
 
     @Test public void agarrarCartaCon2Fichas(){
-        assertEquals( 13, juego3Jugadores().usarFicha().usarFicha().agarrarCarta().getJugadorActual().fichas() );
+        Assertions.assertEquals( 13, juego3Jugadores().pagarFicha(jugador1()).pagarFicha(jugador2()).tomarCarta(jugador3()).getJugadorActual().fichas() );
     }
 
     @Test public void agarrarCartaCon3Fichas(){ //Da una vuelta
-        assertEquals( 13,
-                juego3Jugadores().usarFicha().usarFicha().usarFicha().agarrarCarta().getJugadorActual().fichas() );
+        Assertions.assertEquals( 13,
+                juego3Jugadores().pagarFicha(jugador1()).pagarFicha(jugador2()).pagarFicha(jugador3()).tomarCarta(jugador4()).getJugadorActual().fichas() );
     }
 
 
-    @Test public void noSePuedeUsarFichaSinFichas(){
+    @Test public void noSePuedepagarFichaSinFichas(){
         assertThrowsLike( "No hay fichas",
-                () -> new Jugador("jugador").asignarFichas(0).usarFicha() );
+                () -> new Juego( mazo5Cartas(), 0, tresJugadores()).pagarFicha(jugador1()));
     }
 
     private static void assertThrowsLike( String expectedMsg, Executable expression ){
@@ -101,62 +111,60 @@ public class JuegoTest {
     }
 
     private Juego juego3Jugadores(){
-        return new Juego(tresJugadores(), mazo33Cartas());
+        return new Juego( mazo33Cartas(), 2, tresJugadores() );
     }
 
 
     private Juego juego6Jugadores(){
-        return new Juego(seisJugadores(), mazo33Cartas());
+        return new Juego( mazo33Cartas(), 2 , seisJugadores());
     }
 
 
     private Juego juego7Jugadores() {
-        return new Juego(sieteJugadores(), mazo33Cartas());
+        return new Juego( mazo33Cartas(), 2, sieteJugadores());
     }
 
-    private static Jugador jugador1() {
-        return new Jugador("Jugador1");
+    private static String jugador1() {
+        return "Jugador1";
     }
 
-    private static Jugador jugador2() {
-        return new Jugador("Jugador2");
+    private static String jugador2() {
+        return "Jugador2";
     }
 
-    private static Jugador jugador3() {
-        return new Jugador("Jugador3");
+    private static String jugador3() {
+        return "Jugador3";
     }
 
-    private static Jugador jugador4() {
-        return new Jugador("Jugador4");
+    private static String jugador4() {
+        return "Jugador4";
     }
 
-    private static Jugador jugador5() {
-        return new Jugador("Jugador5");
+    private static String jugador5() {
+        return "Jugador5";
     }
 
-    private static Jugador jugador6() {
-        return new Jugador("Jugador6");
+    private static String jugador6() {
+        return "Jugador6";
     }
 
-    private static Jugador jugador7() {
-        return new Jugador("Jugador7");
+    private static String jugador7() {
+        return "Jugador7";
     }
 
-    private static List<Jugador> tresJugadores(){
+    private static List<String> tresJugadores(){
         return new ArrayList<>(Arrays.asList(jugador1(), jugador2(), jugador3()));
     }
 
-    private static List<Jugador> seisJugadores(){
-        List<Jugador> jugadores = tresJugadores();
+    private static List<String> seisJugadores(){
+        List<String> jugadores = tresJugadores();
         jugadores.addAll(Arrays.asList(jugador4(), jugador5(), jugador6()));
         return jugadores;
     }
 
-    private static List<Jugador> sieteJugadores(){
-        List<Jugador> jugadores = seisJugadores();
+    private static List<String> sieteJugadores(){
+        List<String> jugadores = seisJugadores();
         jugadores.add(jugador7());
         return jugadores;
     }
-
 }
-
