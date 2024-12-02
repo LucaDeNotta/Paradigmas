8661f5f79fb89413d7b722dbd13b55dd2f28912a
LucaDeNotta <141259976+LucaDeNotta@users.noreply.github.com>
2024-12-02 04:18:39 -0300

    Update Juego.java

--- TP 4/Juego.java
+++ TP 4/Juego.java
@@ -2,42 +2,56 @@ package nogracias;
 
 import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public  class Juego {
 
+    public static final String jugadorNoEncontrado = "Jugador no encontrado: ";
     public static String cantidadDeFichasInvalida = "Cantidad de fichas invalida";
     public static String cantidadDeJugadoresNoEsCorrecto = "Cantidad de jugadores no es correcta";
 
-    Jugador jugadorActual;
-    Stack<Carta> mazo;
+    private List<JugadorActivo> jugadores;
+    private EstadoJugador jugadorActual;
+    private Stack<Carta> mazo;
 
     public Juego (Stack<Carta> mazo, int fichasARepartir, List<String> jugadores){
         if (jugadores.size() < 3 || jugadores.size() > 7){
             throw new RuntimeException(cantidadDeJugadoresNoEsCorrecto);
         }
-        if (fichasARepartir < 0){
+        if (fichasARepartir <= 0){
             throw new RuntimeException(cantidadDeFichasInvalida);
         }
 
-//        jugadorActual = jugadores.stream()
-//                .map(jugador -> new JugadorActivo(jugador, fichasARepartir, ) {
-//                });
-        this.jugadorActual = new JugadorActivo(jugadores.getFirst(), fichasARepartir);
+        this.jugadores = jugadores.stream()
+                .map(jugador -> new JugadorActivo(jugador, fichasARepartir) )
+                .collect(Collectors.toList());
+
+        IntStream.range(0, jugadores.size())
+                .forEach(i -> {
+                    JugadorActivo actual = this.jugadores.get(i);
+                    JugadorActivo siguiente = this.jugadores.get((i + 1) % this.jugadores.size());
+                    actual.asignarJugadorSiguiente(siguiente);
+                });
+
+        this.jugadorActual = this.jugadores.getFirst();
         this.mazo = mazo;
     }
 
     public Juego tomarCarta(String jugador){
-        jugadorActual.tomarCarta( jugador, mazo.pop() );
+        jugadorActual = jugadorActual.tomarCarta( jugador, mazo);
         return this;
     }
 
     public Juego pagarFicha(String jugador){
-        jugadorActual.pagarFicha(jugador, mazo.peek());
-
+        jugadorActual = jugadorActual.pagarFicha(jugador, mazo);
         return this;
     }
 
-    public Jugador getJugadorActual() {
-        return jugadorActual;
+    public Integer puntaje (String unJugador) {
+        return jugadores.stream()
+                .filter(jugador -> jugador.esJugador(unJugador))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(jugadorNoEncontrado + unJugador)).puntaje();
     }
 }
